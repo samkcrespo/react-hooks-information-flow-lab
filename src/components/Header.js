7009fd4e1c73051cb06001dc9a4c1cc651c975ad
Samantha Crespo <samkcrespo@gmail.com>
2022-04-28 14:46:36 -0700

    commit tree 6c4ad60f42f84203fc6cd1493796066a8cd43839

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header({ isDarkMode, onDarkModeClick }) {
+  return (
+    <>
+      <h2>Shopster</h2>
+      <button onClick={onDarkModeClick}>
+        {isDarkMode ? "Dark" : "Light"} Mode
+      </button>
+    </>
+  );
+}
+
+export default Header;
